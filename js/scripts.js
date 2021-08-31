d62a6e63a233143316385c2bff5333cc0ea5e6c1
Luis <lfmnovaes@gmail.com>
2021-08-31 11:58:10 -0300

    Add modal prototype
    * Add style for modal
    * Add initial js interaction with modal

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,21 @@ menuLinks.forEach(
     menuLink.addEventListener('click', toggleMenu);
   },
 );
+
+const modal = document.getElementById('myModal');
+const btn = document.getElementById('btn1');
+const span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
